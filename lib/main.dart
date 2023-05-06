3090ad077472b6b5068ec8117f37f172698aed56
alias-73 <alias.akhavanmahdavi@gmail.com>
2023-05-06 21:52:12 +0430

    newCommit

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,34 @@ import 'a_sima_app/models/agentModel.dart';
 
 Future<void> main() async {
   runApp(MyApp());
-  }
+}
 
 class MyApp extends StatefulWidget {
-  @override State<StatefulWidget> createState() => new sampleState();
-  }class sampleState extends State<MyApp> {@override Widget build(BuildContext context) {
+  @override
+  State<StatefulWidget> createState() => new sampleState();
+}
+
+class sampleState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
     List<AgentModel> a = [];
-  return MaterialApp(      
-  navigatorObservers: [FlutterSmartDialog.observer],
-  builder: FlutterSmartDialog.init(),debugShowCheckedModeBanner: false, theme: ThemeData(fontFamily: 'Sans', primaryColor: Colors.white, hintColor: Colors.black,
-   accentColor: Colors.red,
-    primarySwatch: Colors.green,),
-  home: 
+    return MaterialApp(
+      navigatorObservers: [FlutterSmartDialog.observer],
+      builder: FlutterSmartDialog.init(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Sans',
+        primaryColor: Colors.white,
+        hintColor: Colors.black,
+        accentColor: Colors.red,
+        primarySwatch: Colors.green,
+      ),
+      home:
 // jjknk
-//asd adassda asd asd asd
-///asd 
-  Directionality( textDirection: TextDirection.rtl, child: login_page()),
-  );}}
+//asd adassda asd asd asd2
+
+///
+          Directionality(textDirection: TextDirection.rtl, child: login_page()),
+    );
+  }
+}
